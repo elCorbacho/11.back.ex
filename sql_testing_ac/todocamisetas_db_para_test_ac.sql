7e6b41151eefee10280a9d4184edf1427a9ff602
ElCorbacho <histopat.cor@gmail.com>
2025-06-11 12:43:54 -0400

    se corrige sql testing andres

---
+++ sql_testing_ac/todocamisetas_db_para_test_ac.sql
@@ -0,0 +1,124 @@
+
+
+INSERT INTO clientes (nombre_comercial, rut, direccion, categoria, contacto_nombre, contacto_email, porcentaje_oferta) VALUES
+('Zona Fútbol', '10000000-1', 'Av. Gol 101', 'Regular', 'Esteban Vera', 'esteban@zonafutbol.cl', 5.00),
+('Fanáticos Store', '10000001-2', 'Calle Fan 102', 'Preferencial', 'Camila Ruiz', 'camila@fanaticos.cl', 9.00),
+('Jerseys Mundial', '10000002-3', 'Av. Mundial 103', 'Regular', 'Andrés Fuentes', 'andres@jerseysmundial.com', 4.00),
+('Campeones Shop', '10000003-4', 'Pasaje Victoria 104', 'Preferencial', 'María Godoy', 'maria@campeonesshop.cl', 10.00),
+('Estadio Camisetas', '10000004-5', 'Calle Estadio 105', 'Regular', 'Pablo Leiva', 'pablo@estadiocamisetas.cl', 2.50),
+('Soccer Wear', '10000005-6', 'Av. Soccer 106', 'Preferencial', 'Natalia Sáez', 'natalia@soccerwear.com', 7.50),
+('Hincha Gear', '10000006-7', 'Calle Apoyo 107', 'Regular', 'Ricardo Lara', 'ricardo@hinchagear.cl', 5.75),
+('Fútbol Retro', '10000007-8', 'Av. Nostalgia 108', 'Preferencial', 'Patricia Figueroa', 'patricia@futbolretro.cl', 8.00),
+('Equipados FC', '10000008-9', 'Pasaje Equipo 109', 'Regular', 'Matías Pino', 'matias@equipadosfc.com', 6.00),
+('Tribuna Store', '10000009-0', 'Calle Tribuna 110', 'Preferencial', 'Lorena Ulloa', 'lorena@tribunastore.cl', 11.00),
+('Tercer Tiempo', '10000010-1', 'Av. Golazo 111', 'Regular', 'Jorge Tapia', 'jorge@tercertiempo.cl', 3.25),
+('Camisetas y Más', '10000011-2', 'Calle Merch 112', 'Preferencial', 'Valentina Molina', 'valentina@camisetasyma.cl', 10.25),
+('Estilo Futbolero', '10000012-3', 'Pasaje Estilo 113', 'Regular', 'Cristóbal Núñez', 'cristobal@estilofutbolero.cl', 7.00),
+('Ultra Camisetas', '10000013-4', 'Calle Pasión 114', 'Preferencial', 'Paula Cáceres', 'paula@ultracamisetas.com', 6.25),
+('Fútbol Urbano', '10000014-5', 'Av. Ciudad 115', 'Regular', 'Héctor Herrera', 'hector@futbolurbano.cl', 4.75),
+('Camisetas Express', '10000015-6', 'Calle Express 116', 'Preferencial', 'Carolina Díaz', 'carolina@camisetasexp.cl', 12.00),
+('Jersey Fan', '10000016-7', 'Pje. Fanático 117', 'Regular', 'Tomás Reyes', 'tomas@jerseyfan.cl', 6.75),
+('Pasión Camisetas', '10000017-8', 'Av. Amor 118', 'Preferencial', 'Fernanda Soto', 'fernanda@pasioncamisetas.cl', 9.25),
+('Camisetas 24/7', '10000018-9', 'Calle 24 119', 'Regular', 'Gabriel Pinto', 'gabriel@camisetas247.cl', 5.50),
+('El Club Shop', '10000019-0', 'Av. Club 120', 'Preferencial', 'Antonia Sepúlveda', 'antonia@elclubshop.cl', 8.25);
+
+
+
+INSERT INTO camisetas (titulo, club, pais, tipo, color, precio, detalles, codigo_producto) VALUES
+('Camiseta Oficial Uruguay 2024', 'Uruguay', 'Uruguay', 'Oficial', 'Celeste', 79.00, 'Edición Copa América 2024', 'URU-2024'),
+('Camiseta Alternativa Francia 2024', 'Francia', 'Francia', 'Alternativa', 'Azul y Blanco', 82.00, 'Diseño alternativo oficial', 'FRA-ALT24'),
+('Camiseta Oficial Italia Euro 2024', 'Italia', 'Italia', 'Oficial', 'Azul', 85.00, 'Modelo oficial Euro 2024', 'ITA-2024'),
+('Camiseta Retro España 2010', 'España', 'España', 'Retro', 'Rojo', 68.00, 'Campeón del mundo 2010', 'ESP-R10'),
+('Camiseta Oficial Estados Unidos 2024', 'EE.UU.', 'EE.UU.', 'Oficial', 'Blanco y Azul', 74.00, 'Selección USA 2024', 'USA-2024'),
+('Camiseta Local Ecuador 2024', 'Ecuador', 'Ecuador', 'Oficial', 'Amarillo', 77.00, 'Camiseta oficial local', 'ECU-LOC24'),
+('Camiseta Entrenamiento Bayern', 'Bayern Múnich', 'Alemania', 'Entrenamiento', 'Rojo', 62.00, 'Entrenamiento temporada 2024', 'BAY-ENT24'),
+('Camiseta Replica Inglaterra', 'Inglaterra', 'Inglaterra', 'Replica', 'Blanco', 70.00, 'Replica oficial selección inglesa', 'ENG-REP'),
+('Camiseta Oficial Bélgica 2024', 'Bélgica', 'Bélgica', 'Oficial', 'Rojo y Negro', 81.00, 'Modelo Eurocopa 2024', 'BEL-2024'),
+('Camiseta Alternativa Japón', 'Japón', 'Japón', 'Alternativa', 'Azul y Rosa', 76.00, 'Alternativa japonesa', 'JPN-ALT'),
+('Camiseta Oficial Suiza 2024', 'Suiza', 'Suiza', 'Oficial', 'Rojo', 75.00, 'Suiza Euro 2024', 'SUI-2024'),
+('Camiseta Retro Holanda 1988', 'Holanda', 'Países Bajos', 'Retro', 'Naranja', 69.00, 'Homenaje a la Euro 88', 'NED-R88'),
+('Camiseta Entrenamiento Arsenal', 'Arsenal', 'Inglaterra', 'Entrenamiento', 'Gris', 61.00, 'Entrenamiento 2024', 'ARS-ENT24'),
+('Camiseta Local Nigeria', 'Nigeria', 'Nigeria', 'Oficial', 'Verde', 78.00, 'Diseño africano 2024', 'NGA-LOC24'),
+('Camiseta Oficial Dinamarca 2024', 'Dinamarca', 'Dinamarca', 'Oficial', 'Rojo', 74.00, 'Modelo sencillo y clásico', 'DEN-2024'),
+('Camiseta Alternativa Canadá', 'Canadá', 'Canadá', 'Alternativa', 'Negro', 70.00, 'Canadá alternativa 2024', 'CAN-ALT24'),
+('Camiseta Replica Polonia', 'Polonia', 'Polonia', 'Replica', 'Blanco y Rojo', 66.00, 'Replica selección polaca', 'POL-REP'),
+('Camiseta Local Suecia 2024', 'Suecia', 'Suecia', 'Oficial', 'Amarillo y Azul', 79.00, 'Camiseta local 2024', 'SWE-LOC24'),
+('Camiseta Retro River Plate', 'River Plate', 'Argentina', 'Retro', 'Blanco y Rojo', 65.00, 'Temporada 1986', 'RIV-R86'),
+('Camiseta Oficial Escocia 2024', 'Escocia', 'Escocia', 'Oficial', 'Azul Oscuro', 73.00, 'Modelo clásico escocés', 'SCO-2024');
+
+
+
+INSERT INTO `tallas` (`id`, `talla`) VALUES
+(3, 'L'),
+(2, 'M'),
+(1, 'S'),
+(4, 'XL'),
+(5, 'XXL'),
+(6, 'XXXL');
+
+
+
+INSERT INTO camiseta_talla (camiseta_id, talla_id) VALUES
+-- Camiseta 1: S, M, L
+(1, 1), (1, 2), (1, 3),
+-- Camiseta 2: M, L, XL
+(2, 2), (2, 3), (2, 4),
+-- Camiseta 3: S, L, XXL
+(3, 1), (3, 3), (3, 5),
+-- Camiseta 4: M, XL, XXXL
+(4, 2), (4, 4), (4, 6),
+-- Camiseta 5: L, XL, XXL
+(5, 3), (5, 4), (5, 5),
+-- Camiseta 6: S, M, L, XL
+(6, 1), (6, 2), (6, 3), (6, 4),
+-- Camiseta 7: M, L, XXL
+(7, 2), (7, 3), (7, 5),
+-- Camiseta 8: S, M, L, XL, XXL
+(8, 1), (8, 2), (8, 3), (8, 4), (8, 5),
+-- Camiseta 9: S, L, XXXL
+(9, 1), (9, 3), (9, 6),
+-- Camiseta 10: M, L, XL
+(10, 2), (10, 3), (10, 4),
+-- Camiseta 11: M, XL, XXL
+(11, 2), (11, 4), (11, 5),
+-- Camiseta 12: S, M, L
+(12, 1), (12, 2), (12, 3),
+-- Camiseta 13: M, L, XL, XXL
+(13, 2), (13, 3), (13, 4), (13, 5),
+-- Camiseta 14: S, M, L
+(14, 1), (14, 2), (14, 3),
+-- Camiseta 15: L, XL, XXL, XXXL
+(15, 3), (15, 4), (15, 5), (15, 6),
+-- Camiseta 16: S, M, L, XL
+(16, 1), (16, 2), (16, 3), (16, 4),
+-- Camiseta 17: M, L, XXL
+(17, 2), (17, 3), (17, 5),
+-- Camiseta 18: S, M, L, XL, XXL
+(18, 1), (18, 2), (18, 3), (18, 4), (18, 5),
+-- Camiseta 19: S, M, L
+(19, 1), (19, 2), (19, 3),
+-- Camiseta 20: M, L, XL
+(20, 2), (20, 3), (20, 4);
+
+
+INSERT INTO ofertas (cliente_id, camiseta_id, precio_oferta) VALUES
+(1, 1, 75.05),   -- 79 - 5%
+(2, 2, 74.62),   -- 82 - 9%
+(3, 3, 81.60),   -- 85 - 4%
+(4, 4, 61.20),   -- 68 - 10%
+(5, 5, 72.15),   -- 74 - 2.5%
+(6, 6, 71.23),   -- 77 - 7.5%
+(7, 7, 58.43),   -- 62 - 5.75%
+(8, 8, 64.40),   -- 70 - 8%
+(9, 9, 76.14),   -- 81 - 6%
+(10, 10, 67.64), -- 76 - 11%
+(11, 11, 72.56), -- 75 - 3.25%
+(12, 12, 61.95), -- 69 - 10.25%
+(13, 13, 56.97), -- 61 - 7%
+(14, 14, 73.19), -- 78 - 6.25%
+(15, 15, 70.49), -- 74 - 4.75%
+(16, 16, 61.60), -- 70 - 12%
+(17, 17, 61.53), -- 66 - 6.75%
+(18, 18, 71.61), -- 79 - 9.25%
+(19, 19, 61.43), -- 65 - 5.5%
+(20, 20, 66.99); -- 73 - 8.25%
